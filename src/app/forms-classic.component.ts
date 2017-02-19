46a102590cb34807d37756c39a84f27bd7a2b503
Carl <carl.wu67@gmail.com>
2017-02-19 11:42:25 +0800

    ngModel與jsondata綁定

--- src/app/forms-classic.component.ts
+++ src/app/forms-classic.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsClassicComponent implements OnInit {
   forms:Formclass=new Formclass();
+  formdata:any={
+  "RoundedCorners": "ABC",
+  "group1": {
+    "Withhelp": "123",
+    "Labelfocus": "456"
+  }
+};
   constructor() {
 
 
